ab65fa2ffc4d54ab2b2267f0b296327b4ed8ea09
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-07-11 14:30:58 +0400

    style:background added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import Main from "./components/Main";
 function App() {
-  return <></>;
+  return <>
+<Header></Header>
+<Main></Main>
+  </>;
 }
 
 export default App;
