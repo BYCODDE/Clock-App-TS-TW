c961acf1a47bab42f32074edcbb169fe1c5ce68c
Anar Aliev <116018979+BYCODDE@users.noreply.github.com>
2024-07-12 17:15:16 +0400

    refact: api fetch logic added for header!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import refresh from "/desktop/icon-refresh.svg";
 export default function Header() {
-  const [quote, setQuote] = useState(null);
-  
+  const [quote, setQuote] = useState([
+    "The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.",
+  ]);
+  const [author, setAuthor] = useState(["Ada Lovelace"]);
+  const fetchQuote = async () => {
+    const response = await fetch(
+      "https://recite.onrender.com/random/quote-from-db"
+    );
+    const data = await response.json();
+    setQuote(data.quote);
+    setAuthor(data.author);
+    console.log(data.author);
+  };
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <header>
-      <div className="flex items-center justify-between">
-        <div>
-          <p>
-            “The science of operations, as derived from mathematics more
-            especially, is a science of itself, and has its own abstract truth
-            and value.”
-          </p>
-          <h3 className="mt-[8px] font-bold">Ada Lovelace</h3>
+      <div className="flex items-center justify-between ">
+        <div className="max-w-[279px]">
+          <p>{quote}</p>
+          <h3 className="mt-[8px] font-bold">{author}</h3>
         </div>
         <img
           className="mb-[60px] cursor-pointer"
           src={refresh}
           alt="refresh_svg"
+          onClick={fetchQuote}
         />
       </div>
     </header>
